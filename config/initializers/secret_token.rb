0b952cc0d00146f98ebed1f8697d918a8253d4cf
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2011-06-08 14:30:00 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resque::Application.config.secret_token = '98842d5b416c3b942df6f5a9b84b235f568c12a839f95d5effcee5b63f1cf4969deadf7190ab148ccdbfb1dc69772260056eeed1c0de4a10e6bca8fceb588062'
